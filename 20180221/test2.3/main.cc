860a399612ec933bf7ad6b1a05b5ae1d3cc8e9f7
khfaith <834403115@qq.com>
2018-02-21 21:27:34 +0800

    add 02 chapter

---
+++ 20180221/test2.3/main.cc
@@ -0,0 +1,21 @@
+ ///
+ /// @file    main.cc
+ /// @author  lemon(haohb13@gmail.com)
+ /// @date    2018-02-21 18:42:57
+ ///
+ 
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+	unsigned u1=10,u2=32;
+	cout<<u1-u2<<endl;
+	cout<<u2-u1<<endl;
+	int v1=10;
+	cout<<v1-u2<<endl;
+	cout<<u2-v1<<endl;
+	cout<<u1-v1<<endl;
+	cout<<v1-u1<<endl;
+}
