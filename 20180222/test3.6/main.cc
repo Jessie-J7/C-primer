d6ab61da65457efca75cc1ab4f615fac9673930d
khfaith <834403115@qq.com>
2018-02-22 19:50:47 +0800

    03 chapter

---
+++ 20180222/test3.6/main.cc
@@ -0,0 +1,21 @@
+ ///
+ /// @file    main.cc
+ /// @author  lemon(haohb13@gmail.com)
+ /// @date    2018-02-22 11:26:42
+ ///
+ 
+#include <iostream>
+#include <cstring>
+using std::cout;
+using std::endl;
+using std::string;
+int main()
+{
+	string s1("some thing");
+	for(auto &c : s1)
+	{
+		c='X';
+	}
+	cout<<s1<<endl;
+	return 0;
+}
